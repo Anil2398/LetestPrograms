3d88a06800e860da33d08eabf244be931c0f42d5
Your Name <you@example.com>
2020-07-19 11:31:38 -0700

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        int x=45;
+        addtoone(x);
+        System.out.println(x);
+    }
+
+    private static void addtoone(int num) {
+        num=num-1;
+    }
+}
